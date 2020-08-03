e0e6f6901cd9c2e80e5a597912a5c4e6a0f0c774
Jonas <jonasdossantos1991@gmail.com>
2020-08-03 19:39:57 -0300

    listagem funcionando!

--- controller/ListarController.js
+++ controller/ListarController.js
@@ -2,165 +2,53 @@
 //  Esse controller tem por objetivo automatizar a criação dos nodes filhos da ul.
 //
 
-init();
-
-function init(){
-    VIEW_DOM = new ListarController();
-    VIEW_DOM.adicionarEstadosComUFNoSelect();
-}
-
-class ListarController{
-
-    constructor(){
-        this.ARRAY_UF = ["AC","AL","AP","AM","BA","CE","DF","ES","GO","MA","MT","MS","MG","PA","PB","PR","PE","PI","RJ","RN","RS","RO","RR","SC","SP","SE","TO"];
-        this.ARRAY_ESTADOS = ["Acre","Alagoas","Amapá","Amazonas","Bahia","Ceará","Distrito Federal","Espírito Santo","Goiás","Maranhão","Mato Grosso","Mato Grosso do Sul","Minas Gerais","Pará","Paraíba","Paraná","Pernambuco","Piauí","Rio de Janeiro","Rio Grande do Norte","Rio Grande do Sul","Rondônia","Roraima","Santa Catarina","São Paulo","Sergipe","Tocantins"]
-    }
-
-    adicionarEstadosComUFNoSelect(){
-        var select = document.querySelector("select");
+    
+ARRAY_UF = ["AC","AL","AP","AM","BA","CE","DF","ES","GO","MA","MT","MS","MG","PA","PB","PR","PE","PI","RJ","RN","RS","RO","RR","SC","SP","SE","TO"];
+ARRAY_ESTADOS = ["Acre","Alagoas","Amapá","Amazonas","Bahia","Ceará","Distrito Federal","Espírito Santo","Goiás","Maranhão","Mato Grosso","Mato Grosso do Sul","Minas Gerais","Pará","Paraíba","Paraná","Pernambuco","Piauí","Rio de Janeiro","Rio Grande do Norte","Rio Grande do Sul","Rondônia","Roraima","Santa Catarina","São Paulo","Sergipe","Tocantins"]
+var select = document.querySelector("select");
+// -- Adicionar preencher o select //
+    for(i = 0; i<ARRAY_ESTADOS.length;i++){
         var option = document.createElement("option");
-        option.setAttribute("value","JN");
-        option.appendChild(document.createTextNode("Jonas"));
+        option.setAttribute("value",ARRAY_UF[i]);
+        option.appendChild(document.createTextNode(ARRAY_ESTADOS[i]));
         select.appendChild(option);    
     }
 
-    
-}
+// -- Preencher a ul.
+
+var ul = document.querySelector("ul");
 
 
-/*
+var li = document.createElement("li");
+li.classList.toggle("list-group-item");
+li.classList.toggle("list-group-action");
+// cria a header para adicionar o strong e span.
+var header = document.createElement("header");
+header.classList.toggle("d-flex");
+header.classList.toggle("flex-row")
 
+//cria o elemento strong com o nome do local
+var strong = document.createElement("strong");
+strong.classList.toggle("align-items-start");
+strong.appendChild(document.createTextNode("Jonas, funcionou!"));
 
-class Foco{
-    constructor(cpf,cep,descricao){
-        this.cpf = cpf;
-        this.cep = cep;
-        this.uf  = "";
-        this.descricao = descricao;
-        this.status = true;
-    } 
-    setDownStatus(){
-        if(this.quantidade===0)
-            this.status = false;
-        else
-            this.quantidade--;
-    }
-   
-    setUFE(uf){
-        this.uf = uf;
-    }
-}
+//Cria o elemento span com a quantidade de alertas criados.
+var span = document.createElement("span");
+span.classList.toggle("ml-2");
+span.classList.toggle("badge");
+span.classList.toggle("badge-warning");
+span.classList.toggle("align-items-end");
+span.appendChild(document.createTextNode("100"));
 
-init();
+//cria a main para adicionar a tag small com a descrição.
+var main = document.createElement("main");
+main.classList.toggle("flex-row");
+var small = document.createElement("small");
+small.appendChild(document.createTextNode("Não pegou mais vai pegar fogo bixo!"));
 
-function init(){
-   p = new Foco("1111.222.222.22","20040007","pegou fogo");
-   p.setUFE("rj");
-   adicionarLista(p);
-}
-
-
-if(localStorage.getItem("pessoas")!=null){
-    var arrayPessoa = JSON.parse(localStorage.getItem("pessoas"));
-    var arrayFoco = JSON.parse(localStorage.getItem("focos"));
-    for(i = 0; i<arrayFoco.length; i++){
-        adicionarLista(arrayFoco[i]);
-    }
-}
-else 
-{
-    window.location = "../views/main.html";
-    alert("Não há nenhum foco cadastrado!");
-}
-
-
-
-
-function adicionarLista(foco){
-
-    var main_list = document.querySelector("ul");
-    main_list.appendChild(criarLi(foco));
-}
-
-
-
-
-
-
-function criarLi(objFoco){
-    var li = document.createElement('li');
-    li.classList.toggle("list-group-item");
-    li.classList.toggle("list-group-item-action");
-    li.appendChild(criarDivPrincipal(objFoco))
-    return li;
-}
-
-function criarDivPrincipal(objFoco){
-    var div = document.createElement('div'); //d-flex flex-column
-    div.classList.toggle("d-flex");
-    div.classList.toggle("flex-column");
-    div.appendChild(criarMain(objFoco));
-    return div;
-}
-
-
-
-function criarMain(objFoco){
-    var main = document.createElement('main');
-    main.criarSubDivDoMain(objFoco);
-    return main;
-}
-
-function criarSubDivDoMain(objFoco){
-    var subDiv = document.createElement('div'); // justify-content-between
-    subDiv.classList.toggle("d-flex");
-    subDiv.classList.toggle("justify-content-between");
-    subDiv.appendChild(criarSubDivComONomeDoLocal(objFoco.cep,objFoco.descricao));
-    subDiv.appendChild(criarItemcomBadge(contagem(objFoco.uf)));
-    return subDiv;
-
-}
-
-function criarSubDivComONomeDoLocal(local,info){
-    var div = document.createElement('div');
-    div.classList.toggle("d-flex");
-    div.classList.toggle("flex-column");
-
-    var strong = document.createElement('strong');
-    strong.appendChild(document.createTextNode(local));
-    var small = document.createElement('small');
-    small.appendChild(document.createTextNode(info));
-
-    subItem1.appendChild(strong);
-    subItem1.appendChild(small);
-
-    return subItem1;
-
-}
-
-function criarItemcomBadge(q){
-    var div = document.createElement('div');
-    div.classList.toggle("d-flex");
-    div.classList.toggle("flex-column");
-    b = document.createElement('span');
-    b.classList.toggle("badge");
-    b.classList.toggle("badge-warning")
-    b.appendChild(document.createTextNode(q));
-    div.appendChild(b);
-    return div;
-
-}
-
-function contagem(UF){
-    var total = 0;
-    if(localStorage.getItem("pessoas")!=null){
-        var tmp = localStorage.getItem("pessoas");
-        var pessoa = JSON.parse(tmp);
-        for(i = 0; i<pessoa.length; i++){       
-            if(UF===pessoa[i].cep.toLowerCase()){
-            total++;
-            }
-        }
-    }
-    return total;
-}*/
+main.appendChild(small);
+header.appendChild(strong);
+header.appendChild(span);
+li.appendChild(header);
+li.appendChild(main);
+ul.appendChild(li);
