982b7fbf4191777027473d411dc863aa475f4b80
Jonas <jonasdossantos1991@gmail.com>
2020-08-06 16:10:51 -0300

    :o

--- controller/ListarController.js
+++ controller/ListarController.js
@@ -57,22 +57,11 @@ class NovaLista{
    
 }
 
-    var clicked_state = sessionStorage.getItem("clicked_state");
-    ARRAY_UF = ["AC","AL","AP","AM","BA","CE","DF","ES","GO","MA","MT","MS","MG","PA","PB","PR","PE","PI","RJ","RN","RS","RO","RR","SC","SP","SE","TO"];
-    ARRAY_ESTADOS = ["Acre","Alagoas","Amapá","Amazonas","Bahia","Ceará","Distrito Federal","Espírito Santo","Goiás","Maranhão","Mato Grosso","Mato Grosso do Sul","Minas Gerais","Pará","Paraíba","Paraná","Pernambuco","Piauí","Rio de Janeiro","Rio Grande do Norte","Rio Grande do Sul","Rondônia","Roraima","Santa Catarina","São Paulo","Sergipe","Tocantins"]
     var select = document.querySelector("select");
-// -- Adicionar preencher o select //
-    for(i = 0; i<ARRAY_ESTADOS.length;i++){
-        var option = document.createElement("option");
-        option.setAttribute("value",ARRAY_UF[i]);
-        if(clicked_state.toLowerCase() === ARRAY_UF[i].toLowerCase())
-            option.setAttribute("selected","");
-
-        option.appendChild(document.createTextNode(ARRAY_ESTADOS[i]));
-        select.appendChild(option);    
-    }
+    preencherSelect(sessionStorage.getItem("clicked_state"));
 
     preencherUL(clicked_state);
+
 // -- Preencher a ul.
     select.addEventListener("click",function(evento){
         var tag = evento.target;
--- controller/Manipulador.js
+++ controller/Manipulador.js
@@ -76,3 +76,19 @@ function preencherUL(uf){
         
     }
 }
+
+function preencherSelect(selected){
+    var select = document.querySelector("select");
+    ARRAY_UF = ["AC","AL","AP","AM","BA","CE","DF","ES","GO","MA","MT","MS","MG","PA","PB","PR","PE","PI","RJ","RN","RS","RO","RR","SC","SP","SE","TO"];
+    ARRAY_ESTADOS = ["Acre","Alagoas","Amapá","Amazonas","Bahia","Ceará","Distrito Federal","Espírito Santo","Goiás","Maranhão","Mato Grosso","Mato Grosso do Sul","Minas Gerais","Pará","Paraíba","Paraná","Pernambuco","Piauí","Rio de Janeiro","Rio Grande do Norte","Rio Grande do Sul","Rondônia","Roraima","Santa Catarina","São Paulo","Sergipe","Tocantins"]
+    // -- Adicionar preencher o select //
+    for(i = 0; i<ARRAY_ESTADOS.length;i++){
+        var option = document.createElement("option");
+        option.setAttribute("value",ARRAY_UF[i]);
+        if(selected.toLowerCase() === ARRAY_UF[i].toLowerCase())
+            option.setAttribute("selected","");
+
+        option.appendChild(document.createTextNode(ARRAY_ESTADOS[i]));
+        select.appendChild(option);    
+    }
+}
